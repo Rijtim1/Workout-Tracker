5906b7c0d4c97463e8db93f9a6690e9d5cfda72d
Rijan Timsina <regiontimsina@gmail.com>
2024-07-29 19:19:49 -0400

    add middleware

--- backend-workout-tracker/src/main.py
+++ backend-workout-tracker/src/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from src.routers import exercises, users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router, prefix="/api/user", tags=["users"])
 app.include_router(exercises.router, prefix="/api/exercise", tags=["exercises"])
