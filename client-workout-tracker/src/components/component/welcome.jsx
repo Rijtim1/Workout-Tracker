25135aa24a42a281f97879c0ddfcf87ff881479f
Rijan Timsina <regiontimsina@gmail.com>
2024-07-30 09:58:01 -0400

    Update welcome.jsx

--- client-workout-tracker/src/components/component/welcome.jsx
+++ client-workout-tracker/src/components/component/welcome.jsx
@@ -1,7 +1,7 @@
 export function Welcome() {
   return (
     <div className="space-y-2 text-center">
-      <h1 className="text-3xl font-bold tracking-tight">Welcome to Our Website</h1>
+      <h1 className="text-3xl font-bold tracking-tight">Workout Tracker</h1>
       <p className="text-muted-foreground">This is the login and registration page</p>
     </div>
   );
