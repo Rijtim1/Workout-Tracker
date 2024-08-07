6273eadaaff68fe4e64b441190eed0e4637efdcd
Rijan Timsina <regiontimsina@gmail.com>
2024-08-07 14:08:55 -0400

    indentation

--- client-workout-tracker/src/components/hooks/useAuth.js
+++ client-workout-tracker/src/components/hooks/useAuth.js
@@ -7,31 +7,31 @@ import { useRouter } from 'next/navigation'; // Use the correct navigation hook
 import { jwtDecode } from 'jwt-decode'; // Correct import to use named import
 
 const useAuth = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
+  useEffect(() => {
+    const token = localStorage.getItem('token');
 
-        if (!token) {
-            router.push('/'); // Redirect to home page if no token found
-            return;
-        }
+    if (!token) {
+      router.push('/'); // Redirect to home page if no token found
+      return;
+    }
 
-        try {
-            const decodedToken = jwtDecode(token);
-            const currentTime = Date.now() / 1000; // Current time in seconds
+    try {
+      const decodedToken = jwtDecode(token);
+      const currentTime = Date.now() / 1000; // Current time in seconds
 
-            if (decodedToken.exp < currentTime) {
-                // Token expired
-                localStorage.removeItem('token'); // Clear the token from local storage
-                router.push('/'); // Redirect to home page
-            }
-        } catch (error) {
-            console.error('Invalid token:', error);
-            localStorage.removeItem('token'); // Clear the token from local storage
-            router.push('/'); // Redirect to home page
-        }
-    }, [router]); // Dependency array to prevent unnecessary re-runs
+      if (decodedToken.exp < currentTime) {
+        // Token expired
+        localStorage.removeItem('token'); // Clear the token from local storage
+        router.push('/'); // Redirect to home page
+      }
+    } catch (error) {
+      console.error('Invalid token:', error);
+      localStorage.removeItem('token'); // Clear the token from local storage
+      router.push('/'); // Redirect to home page
+    }
+  }, [router]); // Dependency array to prevent unnecessary re-runs
 };
 
 export default useAuth;
