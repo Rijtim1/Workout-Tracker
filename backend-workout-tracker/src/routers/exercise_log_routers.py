987640a25d168a6c0cc15ed48d0552e9a0fc5932
Rijan Timsina <regiontimsina@gmail.com>
2024-08-24 15:31:16 -0400

    add logging

--- backend-workout-tracker/src/routers/exercise_log_routers.py
+++ backend-workout-tracker/src/routers/exercise_log_routers.py
@@ -10,12 +10,13 @@ from src.services.exercise_log_service import (
 )
 from src.routers.users_routers import get_current_user
 from src.schemas.user_schema import User
-
+import logging
 router = APIRouter()
 
 
 @router.post("/", response_model=ExerciseLog)
 async def create_exercise_log_endpoint(log: ExerciseLog, current_user: User = Depends(get_current_user)):
+    logging.debug(f"Received exercise log data: {log.dict()}")
     log.user_id = current_user.id
     return await create_exercise_log(log)
 
--- backend-workout-tracker/src/schemas/exercise_log_schema.py
+++ backend-workout-tracker/src/schemas/exercise_log_schema.py
@@ -22,14 +22,3 @@ class ExerciseLog(BaseModel):
         from_attributes = True
         populate_by_name = True
         json_encoders = {ObjectId: str}
-        json_schema_extra = {
-            "example": {
-                "user_id": "64f19a22f1c79b002ea9b456",
-                "exercise_id": "64f19a22f1c79b002ea9b123",
-                "date": "2024-08-15T14:15:22Z",
-                "sets": 4,
-                "reps": 10,
-                "weight": 50.5,
-                "notes": "Felt strong, increase weight next time"
-            }
-        }
